13edbc3ba704265767a2bad1178f56fa75953f96
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-06-05 18:24:24 -0400

    Adding a "role" to the profile navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <header className="bg-gray-800 md:sticky top-0 z-10">
+        <header className="bg-gray-800 md:sticky top-0 z-10" role="navigation">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="title-font font-medium text-white mb-4 md:mb-0">
                     <a href="#about" className="ml-3 text-xl hover:text-green-500 ease-in duration-300">
